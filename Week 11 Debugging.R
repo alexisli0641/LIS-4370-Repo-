a84cbf0a1258494bbc735db1f5584e0685c45f09
Alexis Li <alexisli0641@gmail.com>
2025-11-10 04:25:39 -0500

    module11_debug.R

---
+++ Week 11 Debugging.R
@@ -0,0 +1,22 @@
+corrected_tukey <- function(x) {
+  outliers <- array(TRUE, dim = dim(x))
+  for (j in seq_len(ncol(x))) {
+    outliers[, j] <- outliers[, j] & tukey.outlier(x[, j])
+  }
+  outlier.vec <- logical(nrow(x))
+  for (i in seq_len(nrow(x))) {
+    outlier.vec[i] <- all(outliers[i, ])
+  }
+  outlier.vec
+}
+
+set.seed(123)
+test_mat <- matrix(rnorm(50), nrow = 10)
+
+
+result <- corrected_tukey(test_mat)
+
+print(result)
+
+
+
